5fa9c5858bba4f9d6265463ab49096509065536d
“Dana Elliott” <delliott123@comcast.net>
2018-10-16 21:28:25 -0400

    updated baseurl for production add eslintrc & scrollBehavior to router

--- src/router.js
+++ src/router.js
@@ -14,5 +14,14 @@ export default new Router({
     {path: '/portfolio', component: Portfolio },
     {path: '/market', component: Market},
     {path: '*', component: Home}
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+        return savedPosition;
+    } else if (to.hash) {
+        return { selector: to.hash};
+    } else {
+        return {x: 0, y: 0};
+    }
+  }
 })
